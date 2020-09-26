72a0627257448dc953906b44eb7c27fd5940a45b
Jacob Corum <cor15031@byui.edu>
2020-09-25 22:09:44 -0600

    added success page

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -5,22 +5,31 @@ import { useStoreContext } from "../../utils/GlobalState";
 import React, { useEffect } from "react";
 import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from "../../utils/actions";
 import { idbPromise } from "../../utils/helpers";
+import { QUERY_CHECKOUT } from "../../utils/queries";
+import { loadStripe } from "@stripe/stripe-js";
+import { useLazyQuery } from "@apollo/react-hooks";
+
+const stripePromise = loadStripe("pk_test_TYooMQauvdEDq54NiTphI7jx");
 
 const Cart = () => {
   const [state, dispatch] = useStoreContext();
+
+  const [getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT);
+
   useEffect(() => {
     async function getCart() {
       const cart = await idbPromise("cart", "get");
       dispatch({ type: ADD_MULTIPLE_TO_CART, products: [...cart] });
     }
-
     if (!state.cart.length) {
       getCart();
     }
   }, [state.cart.length, dispatch]);
+
   function toggleCart() {
     dispatch({ type: TOGGLE_CART });
   }
+
   function calculateTotal() {
     let sum = 0;
     state.cart.forEach((item) => {
@@ -28,6 +37,27 @@ const Cart = () => {
     });
     return sum.toFixed(2);
   }
+
+  function submitCheckout() {
+    const productIds = [];
+    state.cart.forEach((item) => {
+      for (let i = 0; i < item.purchaseQuantity; i++) {
+        productIds.push(item._id);
+      }
+    });
+    getCheckout({
+      variables: { products: productIds },
+    });
+  }
+
+  useEffect(() => {
+    if (data) {
+      stripePromise.then((res) => {
+        res.redirectToCheckout({ sessionId: data.checkout.session });
+      });
+    }
+  }, [data]);
+
   if (!state.cartOpen) {
     return (
       <div className="cart-closed" onClick={toggleCart}>
@@ -37,7 +67,9 @@ const Cart = () => {
       </div>
     );
   }
-  console.log(state)
+
+  console.log(state);
+
   return (
     <div className="cart">
       <div className="close" onClick={toggleCart}>
@@ -52,7 +84,7 @@ const Cart = () => {
           <div className="flex-row space-between">
             <strong>Total: ${calculateTotal()}</strong>
             {Auth.loggedIn() ? (
-              <button>Checkout</button>
+              <button onClick={submitCheckout}>Checkout</button>
             ) : (
               <span>(log in to check out)</span>
             )}
